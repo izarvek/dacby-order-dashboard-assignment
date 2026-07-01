687e437f6ead162773e4d942951970d9abfd0cb3
Vivek Sharma <viveksharmaa6307016893@gmail.com>
2026-07-02 01:13:43 +0530

    feat: add connection to node-cron to schedul automatically

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 import dotenv from "dotenv";
-import connectDB from "./src/config/database.js"
+import connectDB from "./src/config/database.js";
+import startScheduler from "./src/cron/scheduler.cron.js";
 import app from "./src/app.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-await connectDB();
+const startServer = async () => {
+  try {
+    await connectDB();
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+    startScheduler();
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Server failed to start:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
